1c8a316ced29fcd82918ae2ad5110c6a08555d14
Fnu Yiliqi <khai.ilch167@gmail.com>
2021-11-27 22:35:28 -0500

    Created Character Animation system

--- Assets/Scripts/Game/NPCController.cs
+++ Assets/Scripts/Game/NPCController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,7 @@ using UnityEngine;
 public class NPCController : MonoBehaviour, Interactable
 {
     [SerializeField] private Dialog dialog;
+    
     public void Interact()
     {
         StartCoroutine(DialogManager.Instance.ShowDialog(dialog));
